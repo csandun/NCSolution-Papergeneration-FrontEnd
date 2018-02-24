50876d6b9b37332845c2de105912a77719f70bde
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2018-02-24 11:34:51 +0530

    add services files

--- src/app/main/content/exam/exam.module.ts
+++ src/app/main/content/exam/exam.module.ts
@@ -1,12 +1,40 @@
+import { ExamsService } from './exams.service';
+import { ExamService } from './exam.service';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from './../../../core/modules/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExamsComponent } from './exams/exams.component';
 import { ExamComponent } from './exam/exam.component';
 
+const routes = [
+  {
+      path     : 'exams',
+      component: ExamsComponent,
+      resolve  : {
+          academy: ExamsService
+      }
+  },
+  {
+      path     : 'exams/:examId',
+      component: ExamComponent,
+      resolve  : {
+          academy: ExamService
+      }
+  },
+  {
+      path      : '**',
+      redirectTo: 'courses'
+  }
+];
+
 @NgModule({
   imports: [
-    CommonModule
+    SharedModule,
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [ExamsComponent, ExamComponent]
+  declarations: [ExamsComponent, ExamComponent],
+  providers: [ExamService, ExamsService]
 })
 export class ExamModule { }
